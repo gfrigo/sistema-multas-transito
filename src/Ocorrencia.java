ff0364115b8b70a4c07da9f04ca2fcba5ba07232
Breno Amorim Candido <breno11a@hotmail.com>
2024-12-02 14:04:10 -0300

    populando-classe-ocorrencia

--- src/Ocorrencia.java
+++ src/Ocorrencia.java
@@ -2,8 +2,72 @@ import java.util.Date;
 
 public class Ocorrencia {
   private String placa;
-  private Date dataHora;
+  private Date data;
   private String nomeLogradouro;
-  private int velocidadeMedida;
+  private int velocidade;
   private int tipoVeiculo;
+
+  public Ocorrencia(String placa, Date data, String nomeLogradouro, int velocidade, int tipoVeiculo) {
+    this.placa = placa;
+    this.data = data;
+    this.nomeLogradouro = nomeLogradouro;
+    this.velocidade = velocidade;
+    this.tipoVeiculo = tipoVeiculo;
+}
+
+// metodos get e set
+
+  public String getPlaca() {
+    return placa;
+  }
+
+  public void setPlaca(String placa) {
+    this.placa = placa;
+  }
+
+  public Date getData() {
+    return data;
+  }
+
+  public void setData(Date data) {
+    this.data = data;
+  }
+
+  public String getNomeLogradouro() {
+    return nomeLogradouro;
+  }
+
+  public void setNomeLogradouro(String nomeLogradouro) {
+    this.nomeLogradouro = nomeLogradouro;
+  }
+
+  public int getVelocidade() {
+    return velocidade;
+  }
+
+  public void setVelocidade(int velocidade) {
+    this.velocidade = velocidade;
+  }
+
+  public int getTipoVeiculo() {
+    return tipoVeiculo;
+  }
+
+  public void setTipoVeiculo(int tipoVeiculo) {
+    this.tipoVeiculo = tipoVeiculo;
+  }
+
+  //serve para exibir melhor os dados
+  @Override
+  public String toString() {
+    return "Nova Ocorrencia { " +
+            "placa = '" + placa + '\'' +
+            ", data = " + data +
+            ", nomeLogradouro = '" + nomeLogradouro + '\'' +
+            ", velocidade = " + velocidade +
+            ", tipoVeiculo = " + (tipoVeiculo == 0 ? "Veículo leve" : "Caminhão") +
+            '}';
+  }
+
+
 }
