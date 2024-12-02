0080aafa6c4689c771e1b189b9d0ee4289c5bdd0
Breno Amorim Candido <breno11a@hotmail.com>
2024-12-02 14:05:53 -0300

    populando-classe-multa

--- src/Multa.java
+++ src/Multa.java
@@ -5,4 +5,58 @@ public class Multa {
   private String motivo;
   private Date data;
   private String placa;
+
+  public Multa(double valor, String motivo, Date data, String placa){
+    this.valor = valor;
+    this.motivo = motivo;
+    this.data = data;
+    this.placa = placa;
+  }
+
+  // metodos de Get e Set
+  public double getValor() {
+    return valor;
+  }
+
+  public void setValor(double valor){
+    this.valor = valor;
+  }
+
+  public String getMotivo() {
+    return motivo;
+  }
+
+  public void setMotivo(String motivo) {
+    this.motivo = motivo;
+  }
+
+  public Date getData() {
+    return data;
+  }
+
+  public void setData(Date data) {
+    this.data = data;
+  }
+
+  public String getPlaca() {
+    return placa;
+  }
+
+  public void setPlaca(String placa) {
+    this.placa = placa;
+  }
+
+  //serve para exibir melhor os dados
+  @Override
+  public String toString() {
+    return "Multa Aplicada{ " +
+    "valor = " + valor +
+    ", motivo = '" + motivo + '\'' +
+    ", data = " + data +
+    ", placa = '" + placa + '\'' +
+    '}';
+  }
+
 }
+
+
